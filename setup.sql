046fee727ef53f5da5de8b9bbfc999de260ebbc1
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-12-16 13:42:23 -0800

    parser and sql written

---
+++ setup.sql
@@ -0,0 +1,11 @@
+DROP TABLES IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL
+    cover_image TEXT NOT NULL
+    rating BIGINT
+    price BIGINT
+    in_stock BOOLEAN
+);
+
