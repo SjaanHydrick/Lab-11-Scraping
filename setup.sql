09af68dfc38362e8022cc9b4c732710cdda4a12c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-12-16 16:01:37 -0800

    store.js working

--- setup.sql
+++ setup.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
     title TEXT NOT NULL,
     cover_image TEXT NOT NULL,
     rating TEXT NOT NULL,
     price TEXT NOT NULL,
-    in_stock BOOLEAN
+    in_stock BOOLEAN NOT NULL
 );
 
